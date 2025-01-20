74352ccf0ecca729a3b38af26a7be3b5d36db326
GuilhermeHMG <guilherme.ghg00@gmail.com>
2025-01-20 18:45:04 -0300

    Intial Commit node_do_zero v1

---
+++ README.md
@@ -0,0 +1,13 @@
+https://www.youtube.com/watch?v=hHM-hr9q4mo
+
+# DICAS
+
+## Para inicial o servidor node, onde ele ficará assistindo (--watch) as alterações no código sem mostrar alertas (--no-warnings)
+```
+node --watch --no-warnings server.js
+```
+
+## Instalação do framework FASTIFY (Um mini framework para criação de servidores HTTP)
+```
+npm install fastify
+```
---
+++ server.js
@@ -0,0 +1,85 @@
+/**
+ * 1. CRIANDO SERVIDOR COM NODE.JS NATIVO
+ *
+ *
+ * Estamos criando neste arquivo um SERVIDOR NATIVO EM NODE, ou seja, um servidor HTTP sem
+ * nenhum tipo de framework, apenas com o módulo 'http' do Node.js, para fins de aprendizagem.
+ * Mas nenhuma empresa cria um servidor assim, pois é muito trabalhoso e não é escalável. Por isso,
+ * criamos servidores com frameworks, que facilitam a criação de servidores HTTP.
+ */
+
+// // Importa a função createServer do módulo 'http' do Node.js
+// import { createServer } from "node:http";
+
+// // Cria um servidor HTTP
+// const server = createServer((request, response) => {
+//   // Escreve a resposta "oi" para o cliente
+//   response.write("oi");
+
+//   // Finaliza a resposta
+//   return response.end();
+// });
+
+// // Faz o servidor escutar na porta 3333
+// server.listen(3333);
+
+/**
+ * 2. CRIANDO SERVIDOR COM FASTIFY (BEM MELHOR)
+ *
+ *
+ */
+import { fastify } from "fastify";
+import { databaseMemory } from "./database-memory.js";
+import { title } from "node:process";
+
+const server = fastify();
+
+const database = new databaseMemory();
+
+// GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD
+//  Request Body
+
+server.post("/videos", (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title: title,
+    description: description,
+    duration: duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get("/videos", (request) => {
+  const search = request.query.search;
+
+  const videos = database.list(search);
+
+  return videos;
+});
+
+server.put("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  database.update(videoId, {
+    title: title,
+    description: description,
+    duration: duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  port: 3333,
+});
